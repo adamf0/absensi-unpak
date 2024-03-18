d96f1fddeea6a52e0edffdc5015f647b890ff000
!Github@251423! <studiocode52@gmail.com>
2024-03-18 15:06:39 +0700

    modified:   src/page/home.tsx

--- src/page/home.tsx
+++ src/page/home.tsx
@@ -1,65 +1,55 @@
 import '../home.css'
 import agendaIcon from '../assets/agenda-icon.png'
-import { useEffect, useRef, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import { usePopper } from 'react-popper';
 import Welcoming from '../component/welcoming';
 import Absent from '../component/absent';
 import Card from '../component/card';
 import { CutiModel } from '../component/CutiModel';
 
-function Home(){
+function Home() {
     const boxRef = useRef();
     const tooltipRef = useRef();
-    const {styles, attributes} = usePopper(boxRef.current, tooltipRef.current, {
+    const { styles, attributes } = usePopper(boxRef.current, tooltipRef.current, {
         modifiers: [{ name: 'offset', options: { offset: [10, 0] } }],
     });
     const [listData, setListData] = useState([
-        new CutiModel("Kamis, 14 maret 2024",2,"Sakit","Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos obcaecati non temporibus dignissimos quasi beatae doloremque corporis fuga consequatur deserunt?","Pending","1",false),
-        new CutiModel("Kamis, 17 maret 2024",10,"Mudik","Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos obcaecati non temporibus dignissimos quasi beatae doloremque corporis fuga consequatur deserunt?","Pending","2",false)
+        new CutiModel("Kamis, 14 maret 2024", 2, "Sakit", "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos obcaecati non temporibus dignissimos quasi beatae doloremque corporis fuga consequatur deserunt?", "Pending", "1", false),
+        new CutiModel("Kamis, 17 maret 2024", 10, "Mudik", "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos obcaecati non temporibus dignissimos quasi beatae doloremque corporis fuga consequatur deserunt?", "Pending", "2", false)
     ]);
 
-    const toggleDialog = (index: number) =>{
-        const updatedListData = listData.map((data, i) => {
-            if (i === index) {
-                return { ...data, openDetail: true };
-            } else {
-                return { ...data, openDetail: false };
-            }
-        });
-        console.log(updatedListData);
-        setListData(updatedListData);
-    }
+    const toggleDialog = useCallback((index: number) => {
+        setListData(prevListData => prevListData.map((data, i) => ({
+            ...data,
+            openDetail: i === index
+        })));
+    }, []);
 
-    useEffect(() => {
-        // Fungsi untuk menutup dialog saat terjadi klik diluar area popover
-        const handleOutsideClick = (event) => {
-          if (
+    const handleOutsideClick = useCallback((event) => {
+        if (
             boxRef.current &&
             tooltipRef.current &&
             !boxRef.current.contains(event.target) &&
             !tooltipRef.current.contains(event.target)
-          ) {
-            const updatedListData = listData.map((data, i) => {
-                return { ...data, openDetail: false };
-            });
-            console.log(updatedListData);
-            setListData(updatedListData);
-          }
-        };
-    
-        // Tambahkan event listener untuk mendengarkan klik diluar area popover
+        ) {
+            setListData(prevListData => prevListData.map(data => ({
+                ...data,
+                openDetail: false
+            })));
+        }
+    }, []);
+
+    useEffect(() => {
         document.addEventListener('mousedown', handleOutsideClick);
-    
         return () => {
-          // Hapus event listener saat komponen unmount
-          document.removeEventListener('mousedown', handleOutsideClick);
+            document.removeEventListener('mousedown', handleOutsideClick);
         };
-      }, []);
+    }, [handleOutsideClick]);
 
     return (
         <div className="wrapper">
-            <Welcoming/>
-            <Absent/>
+            <Welcoming />
+            <Absent />
 
             <section className="leave card">
                 <div className="leave__content row-container spaceBetweenRow">
@@ -82,13 +72,13 @@ function Home(){
                             {
                                 listData.map((data, index) => {
                                     return <Card
-                                    key={index} 
-                                    data={data} 
-                                    toggleDialog={()=>toggleDialog(index)} 
-                                    boxRef={boxRef} 
-                                    tooltipRef={tooltipRef} 
-                                    styleDialog={{display: data.openDetail ? 'block' : 'none', ...styles.popper}} 
-                                    attributes={attributes}/>;
+                                        key={index}
+                                        data={data}
+                                        toggleDialog={() => toggleDialog(index)}
+                                        boxRef={boxRef}
+                                        tooltipRef={tooltipRef}
+                                        styleDialog={{ display: data.openDetail ? 'block' : 'none', ...styles.popper }}
+                                        attributes={attributes} />;
                                 })
                             }
                         </tbody>
@@ -96,7 +86,7 @@ function Home(){
                 </div>
             </section>
 
-            <div className="modal fades show" id="modalTambah" aria-hidden="true" style={{display: 'none'}}>
+            <div className="modal fades show" id="modalTambah" aria-hidden="true" style={{ display: 'none' }}>
                 <div className="modal-dialog">
                     <div className="modal-content">
                         <div className="modal-header">
@@ -107,7 +97,7 @@ function Home(){
                             <div className="row">
                                 <div className="col-12">
                                     <label className="form-label">Tanggal Pengajuan</label>
-                                    <input type="text" name="tanggal" className="form-control"/>
+                                    <input type="text" name="tanggal" className="form-control" />
                                 </div>
                                 <div className="col-12">
                                     <label className="form-label">Jenis Cuti</label>
@@ -118,7 +108,7 @@ function Home(){
                                 </div>
                                 <div className="col-12">
                                     <label className="form-label">Lama Cuti</label>
-                                    <input type="number" name="lama" className="form-control"/>
+                                    <input type="number" name="lama" className="form-control" />
                                 </div>
                                 <div className="col-12">
                                     <label className="form-label">Tujuan</label>
