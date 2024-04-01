0753f504d2c83a736d15b396b9e9bc6bfdd28ac4
!Github@251423! <studiocode52@gmail.com>
2024-04-01 08:35:36 +0700

    bug

--- src/model/Absen.tsx
+++ src/model/Absen.tsx
@@ -1,1 +1,21 @@
-export class Absen{}
+export class Absen {
+    id: string;
+    nidn: number;
+    tanggal: string;
+    absen_masuk: string;
+    absen_keluar: string;
+    
+    constructor(
+        id: string,
+        nidn: number,
+        tanggal: string,
+        absen_masuk: string,
+        absen_keluar: string
+    ) {
+        this.id = id;
+        this.nidn = nidn;
+        this.tanggal = tanggal;
+        this.absen_masuk = absen_masuk;
+        this.absen_keluar = absen_keluar;
+    }
+  }
