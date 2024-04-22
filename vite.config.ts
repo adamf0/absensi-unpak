8f8c5e241c11665b8a38540886cd79c4a33e0606
!Github@251423! <studiocode52@gmail.com>
2024-04-22 14:32:29 +0700

    modified:   vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => {
 
 		resolve: {
 			alias: {
-			  '@module': path.resolve(__dirname, 'src/module/'),
+			  '@module': path.resolve(__dirname, 'src/module'),
 			},
 		},
 		define: {
