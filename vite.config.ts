46b9792c01035a89577a249dfbedb0e4b1d5cc20
!Github@251423! <studiocode52@gmail.com>
2024-04-22 14:36:01 +0700

    debug

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import EnvironmentPlugin from 'vite-plugin-environment';
-import path from 'node:path';
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => {
 
 		resolve: {
 			alias: {
-			  '@module': path.resolve(__dirname, 'src/module'),
+			  '@module': path.resolve(__dirname, './src/module'),
 			},
 		},
 		define: {
