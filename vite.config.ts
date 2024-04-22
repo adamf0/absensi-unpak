5fcf0994e785bed5937f17d97e09047bd672a5fd
!Github@251423! <studiocode52@gmail.com>
2024-04-22 13:26:59 +0700

    debug config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,17 @@
-import { defineConfig, loadEnv, splitVendorChunkPlugin } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import EnvironmentPlugin from 'vite-plugin-environment';
+import path from 'node:path';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
 	const env = loadEnv(mode, process.cwd(), '');
 	return {
+		resolve:{
+			alias: {
+				'@module': path.resolve(__dirname, './module')
+			}
+		},
 		define: {
 			'process.env.deploy': JSON.stringify(env.deploy),
 			'process.env.base_url_api': JSON.stringify(env.base_url_api)
