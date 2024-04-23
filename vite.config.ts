4d7b48fdb4863d95b5bbd825b4f85a042cca2a68
!Github@251423! <studiocode52@gmail.com>
2024-04-23 09:36:04 +0700

    debug

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path, { join } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-	const env = loadEnv(mode, process.cwd(), '');
+	const env = mode == 'production' ? loadEnv(mode, process.cwd(), '') : {};
 	return {
 		resolve: {
 			alias: [
