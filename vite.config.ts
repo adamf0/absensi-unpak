544c84295064d9c7f76cf4cb51b227f053fc7426
!Github@251423! <studiocode52@gmail.com>
2024-04-23 08:33:41 +0700

    debug

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,27 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import EnvironmentPlugin from 'vite-plugin-environment';
-import * as path from "path";
+import path, { join } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
 	const env = loadEnv(mode, process.cwd(), '');
 	return {
 		resolve: {
-			alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }]
+			alias: [
+				{
+					find: "@",
+					replacement: path.resolve(__dirname, "src"),
+					'@root': join(__dirname, 'src'),
+				},
+			]
 		},
 		define: {
 			'process.env.deploy': JSON.stringify(env.deploy),
 			'process.env.base_url_api': JSON.stringify(env.base_url_api)
 		},
 		assetsInclude: ['**/*.md'],
-		// base: "/",
+		base: "/",
 		plugins: [react(), EnvironmentPlugin({})],
 		preview: {
 			port: 81,
@@ -25,6 +31,10 @@ export default defineConfig(({ mode }) => {
 			port: 81,
 			strictPort: true,
 			host: true,
+			// fs: {
+			// 	cachedChecks: false,
+			// },
+		  
 			// origin: "http://0.0.0.0:81",
 		},
 	}
