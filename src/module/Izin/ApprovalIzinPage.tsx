97e8f9aeb176cca0ba3393b3dccd9d6e03afc403
!Github@251423! <studiocode52@gmail.com>
2024-05-02 08:57:38 +0700

    bug

--- src/module/Izin/ApprovalIzinPage.tsx
+++ src/module/Izin/ApprovalIzinPage.tsx
@@ -160,14 +160,14 @@ const ApprovalIzinPage = () => {
                                         <Td>
                                             {
                                                 item.status==""||item.status=="menunggu"?
-                                                <>
+                                                <div className="flex flex-wrap gap-2">
                                                     <Button variant='solid' className="grow"  color="blue" onClick={()=>setApproval(new Approval(true,item.id,"terima"))}>
                                                         Terima
                                                     </Button>
                                                     <Button variant='solid' className="grow" color="red" onClick={()=>setApproval(new Approval(false,item.id,"tolak"))}>
                                                         Tolak
                                                     </Button>
-                                                </>:null
+                                                </div>:null
                                             }
                                         </Td>
                                     </Tr>
